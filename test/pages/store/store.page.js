5e212b6138627adf625962b49a7f3222c911b47b
gerardbenavides <54349270+gerardbenavides@users.noreply.github.com>
2020-09-02 23:38:48 +0800

    added archive store

--- test/pages/store/store.page.js
+++ test/pages/store/store.page.js
@@ -3,6 +3,8 @@ const Page = require('../page');
 class StorePage extends Page {
     get title () { return $('//div[@class="header-container"]//div[@class="left-container flex-cross-center"]//div[@class="title"]')}
     get btnAddStore () { return $('//button[@class="label button-primary"]')}
+    get tabActive () { return $('//div[@class="left-container flex-cross-center"]//div[@class="tab-container flex"]//span[contains(text(),"Aktiv")]')}
+    get tabArchive () { return $('//div[@class="left-container flex-cross-center"]//div[@class="tab-container flex"]//span[contains(text(),"Arkiv")]')}
     get iconSearch () { return ('//div[@class="action-search icon-container flex-center clickable ng-star-inserted"]//div[@class="icon-svg flex-center"]')}
     get inputSearch () { return ('//input[@placeholder="Sök återförsäljare"]')}
     
@@ -14,6 +16,9 @@ class StorePage extends Page {
 
     /** View Store */
     get btnEditStore () { return $('//div[@class="button-container edit"]//app-button')}
+    get btnArchiveStore () { return $('//div[@class="right-container flex-cross-center"]//app-button[@class="action-delete"]')}
+    get popupContentBody () { return $('//app-dialog-confirm//div[@class="subtitle-container ng-star-inserted"]')}
+    get popupBtnArchive () { return $('//app-dialog-confirm//button[@class="label button-primary button-warning"]')}
     get previewStoreName () { return $('//div[@class="name-container"]//span')}
     get previewStoreNumber () { return $('//div[@class="contact-container"]//span')}
     get previewStoreCity () { return $('//div[@class="content-wrapper flex-main-center"]//div[@class="label-container"]//span')}
@@ -35,6 +40,7 @@ class StorePage extends Page {
 
     editStore(name, city) {
         this.inputStoreName.setValue(name);
+        //browser.pause(1000)
         this.inputStoreCity.setValue(city);
         this.btnSave.click();
     }
--- test/specs/store/store.spec.js
+++ test/specs/store/store.spec.js
@@ -34,24 +34,21 @@ describe('As Admin, I can add a Store', () => {
             storeCity, // Store city parameter
             storeNumber, // Store number parameter
         )
+
+        StorePage.storeLocator(storeName).isDisplayed();
     })
 })
 
-describe.skip('As Admin, I can search the added Store', () => { // BROKEN AND TRASH
+describe('As Admin, I can search the added Store', () => { // BROKEN AND TRASH
     it('Searches for the added store', () => { 
-        HomePage.btnMenuStore.click();
-
-        browser.pause(5000)
-        StorePage.iconSearch.waitForDisplayed();
         StorePage.iconSearch.click();
-        StorePage.inputSearch.waitForDisplayed();
         StorePage.inputSearch.setValue(storeName);
-
     })
 })
 
 describe('As Admin, I can view and validate the added Store\'s details', () => {
     it('Validates added store\'s details', () => {
+        StorePage.storeLocator(storeName).scrollIntoView();
         StorePage.storeLocator(storeName).click();
         expect(StorePage.title).toHaveText("Återförsäljare " + storeName)
         
@@ -59,16 +56,40 @@ describe('As Admin, I can view and validate the added Store\'s details', () => {
     })
 })
 
-describe('As Admin, I can view and validate the added Store\'s details', () => {
+let storeNameEdited = (storeName + "~Edited")
+
+describe('As Admin, I can edit a Store', () => {
     
     it('Edits store\'s name and city', () => {
         StorePage.btnEditStore.click();
-
+        browser.pause(1000) // produces bug if there's no pause
+        
         StorePage.editStore(
-            "~" + storeName, // Store name parameter
-            "~" + storeCity, // Store city parameter
+            storeNameEdited, // Store name parameter
+            storeCity + "~Edited", // Store city parameter
         )
 
-        StorePage.storeLocator("~" + storeName).isDisplayed();
+        StorePage.storeLocator(storeNameEdited).isDisplayed();
+    })
+})
+
+describe('As Admin, I can archive a Store', () => {
+    
+    it('Archives created store', () => {
+        StorePage.storeLocator(storeNameEdited).click();
+
+        StorePage.btnArchiveStore.click();
+        expect(StorePage.popupContentBody).toHaveText("Vill du arkivera denna butik " + storeNameEdited+"?")
+        StorePage.popupBtnArchive.click();
+
+        expect(StorePage.title).toHaveText("Återförsäljare");
+    })
+})
+
+describe('As Admin, I can view the archived Store', () => {
+    
+    it('Navigates to Archive screen', () => {
+        StorePage.tabArchive.click();
+        StorePage.storeLocator(storeNameEdited).isDisplayed();
     })
 })
