5cf58900d606ee8c848085d796e0adb56c3f6932
gerardbenavides <54349270+gerardbenavides@users.noreply.github.com>
2021-03-19 18:34:34 +0800

    Create coupon-data.js

---
+++ data/coupon-data.js
@@ -0,0 +1,36 @@
+const random = require('../test/helpers/random')
+
+module.exports = {
+    name: random.string(), 
+    priority: 1,
+    petType: "Both",
+    description: random.paragraph(),
+    consumerReward: 10,
+    breederReward: 20,
+    hunterReward: 30,
+    consumerCondition: 5,
+    
+    get nameEdited() {
+        return this.name + "~"
+    },
+    get priorityEdited() {
+        return this.priority + 1
+    },
+    get descriptionEdited() {
+        return this.description + "~"
+    },
+    get consumerRewardEdited() {
+        return this.consumerReward + 10
+    },
+    get breederRewardEdited() {
+        return this.breederReward + 10
+    },
+    get hunterRewardEdited() {
+        return this.hunterReward + 10
+    },
+    get consumerConditionEdited() {
+        return this.consumerCondition + 5
+    },
+
+    petTypeEdited: "Dog",
+}
