2957a9548126ebb5e17ff1b9e377b3d2c2812451
gerardbenavides <54349270+gerardbenavides@users.noreply.github.com>
2021-04-07 10:09:06 +0800

    apply object

--- test/specs/store/store.spec.js
+++ test/specs/store/store.spec.js
@@ -1,18 +1,14 @@
 const HomePage = require('../../pages/home/home.page');
 const LoginPage = require('../../pages/auth/login.page');
 const StorePage = require('../../pages/store/store.page');
-const Random = require('../../helpers/random')
 
-let storeName = ("Z9" + Random.string());
-let storeCity = Random.city();
-let storeNumber = Random.integer6();
+let store = require('../../../data/store-data')
+const { credentials } = require('../../../environments/environment-variables')
 
 describe('As Admin, I can login to the web admin portal', () => {
-    it('Logs in Netzon Admin with valid credentials', () => {
-        LoginPage.open();
-
-        LoginPage.login(process.env.STAGING_ADMIN_EMAIL, process.env.STAGING_ADMIN_PASS);
-        
+    it('Should login Netzon Admin with valid credentials', () => {
+        LoginPage.open()
+        LoginPage.login(credentials, 'admin')  
     })
 })
 
@@ -26,36 +22,23 @@ describe('As Admin, I can add a Store', () => {
 
     it('Adds a store', () => {
         StorePage.btnAddStore.click();
-
         expect(StorePage.title).toHaveText("Lägg till återförsäljare");
 
-        StorePage.addStore(
-            storeName, // Store name parameter
-            storeCity, // Store city parameter
-            storeNumber, // Store number parameter
-        )
-
-        StorePage.storeLocator(storeName).isDisplayed();
+        StorePage.addStore(store)
     })
 })
  
 describe('As Admin, I can search the added Store', () => { // BROKEN AND TRASH
     it('Searches for the added store', () => { 
-        StorePage.iconSearch.click();
-        StorePage.inputSearch.setValue(storeName);
+        StorePage.storeLocator(store.name).click()
     })
 })
 
 describe('As Admin, I can view and validate the added Store\'s details', () => {
     it('Validates added store\'s details', () => {
-        StorePage.storeLocator(storeName).click();
-        expect(StorePage.title).toHaveText("Återförsäljare " + storeName)
-        
-        StorePage.validateStore(storeName, storeCity, storeNumber)
+        StorePage.validateStore(store, false)
     })
 })
-
-let storeNameEdited = (storeName + "~Edited")
 
 describe('As Admin, I can edit a Store', () => {
     
@@ -63,31 +46,26 @@ describe('As Admin, I can edit a Store', () => {
         StorePage.btnEditStore.click();
         browser.pause(1000) // produces bug if there's no pause
         
-        StorePage.editStore(
-            storeNameEdited, // Store name parameter
-            storeCity + "~Edited", // Store city parameter
-        )
-
-        StorePage.storeLocator(storeNameEdited).isDisplayed();
+        StorePage.editStore(store)
+    })
+    it('Validates edited store\'s details', () => {
+        StorePage.searchStore(store.nameEdited)
+        StorePage.storeLocator(store.nameEdited).click()
+        StorePage.validateStore(store, true)
     })
 })
 
 describe('As Admin, I can archive a Store', () => {
     
     it('Archives created store', () => {
-        StorePage.iconSearch.click();
-        StorePage.inputSearch.setValue(storeNameEdited);
-        StorePage.storeLocator(storeNameEdited).waitForDisplayed();
-        StorePage.storeLocator(storeNameEdited).click();
-
         StorePage.btnArchiveStore.click();
-        expect(StorePage.popupContentBody).toHaveText("Vill du arkivera denna butik " + storeNameEdited+"?")
+        expect(StorePage.popupContentBody).toHaveText("Vill du arkivera denna butik " + store.nameEdited+"?")
         StorePage.popupBtnArchive.click();
 
     })
     it('Validates that created store is not showing in Active page', () => {
         StorePage.iconSearch.click();
-        StorePage.inputSearch.setValue(storeNameEdited);
+        StorePage.inputSearch.setValue(store.nameEdited);
         StorePage.resultNotFound.isDisplayed();
     })
 
@@ -97,6 +75,6 @@ describe('As Admin, I can view the archived Store', () => {
     
     it('Navigates to Archive screen', () => {
         StorePage.tabArchive.click();
-        StorePage.storeLocator(storeNameEdited).isDisplayed();
+        StorePage.storeLocator(store.nameEdited).isDisplayed();
     })
 })
