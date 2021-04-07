dda303ed08159db8a10a5846be79dc9b3ea6a080
gerardbenavides <54349270+gerardbenavides@users.noreply.github.com>
2021-04-07 10:08:48 +0800

    fix multiple bugs

--- test/pages/store/store.page.js
+++ test/pages/store/store.page.js
@@ -8,7 +8,9 @@ class StorePage extends Page {
     get iconSearch () { return $('/html[1]/body[1]/app-root[1]/div[1]/div[2]/app-store-list[1]/div[1]/div[1]/app-header[1]/div[1]/div[1]/div[2]/div[1]/div[1]')}
     get inputSearch () { return $('//input[@placeholder="Sök återförsäljare"]')}
     get resultNotFound () { return $('//div[@class="placeholder-container flex-center ng-star-inserted"]//span[@class="header-6"]')}
-    
+    get iconClearSearch () { return $('//div[@class="right-container flex-cross-center"]//div[3]/app-icon-svg')}
+    get labelSearchedKeyword () { return $('//div[@class="search-container"]//span[contains(text(),"Söker efter: ")]')}
+
     get inputStoreName () { return $('//input[@placeholder="Skriv in återförsäljare"]')}
     get inputStoreCity () { return $('//input[@placeholder="Skriv in location"]')}
     get inputStoreNumber () { return $('//input[@placeholder="Skriv in kundnummer."]')}
@@ -25,24 +27,44 @@ class StorePage extends Page {
     
     /** METHODS */
 
-    addStore(name, city, number) {
-        this.inputStoreName.setValue(name);
-        this.inputStoreCity.setValue(city);
-        this.inputStoreNumber.setValue(number);
+    addStore(store) {
+        this.inputStoreName.setValue(store.name);
+        this.inputStoreCity.setValue(store.city);
+        this.inputStoreNumber.setValue(store.number);
         this.btnSave.click();
+        this.btnAddStore.waitForClickable()
+        browser.pause(1000)
+        this.searchStore(store.name)
     }
 
-    validateStore(name, city, number) {
-        expect(this.previewStoreName).toHaveText(name);
-        expect(this.previewStoreCity).toHaveText(city);
-        expect(this.previewStoreNumber).toHaveText("Kundnummer. " + number);
+    validateStore(store, isEdited) {
+        if(isEdited) {
+            expect(this.previewStoreName).toHaveText(store.nameEdited)
+            expect(this.previewStoreCity).toHaveText(store.city)
+            expect(this.previewStoreNumber).toHaveText("Kundnummer. " + store.number)
+        } else {
+            expect(this.previewStoreName).toHaveText(store.name)
+            expect(this.previewStoreCity).toHaveText(store.city)
+            expect(this.previewStoreNumber).toHaveText("Kundnummer. " + store.number)
+        }
+
     }
 
-    editStore(name, city) {
-        this.inputStoreName.setValue(name);
+    editStore(store) {
+        this.inputStoreName.setValue(store.nameEdited);
         //browser.pause(1000)
-        this.inputStoreCity.setValue(city);
+        this.inputStoreCity.setValue(store.city);
         this.btnSave.click();
+
+        this.btnAddStore.waitForClickable()
+        browser.pause(1000)
+        //expect(this.storeLocator(store.name)).toBeDisplayed()
+    }
+
+    searchStore (name) {
+        this.iconSearch.click();
+        this.inputSearch.setValue(name)
+        this.storeLocator(name).waitForDisplayed({timeout: 15000})
     }
 
     storeLocator (name) {
--- test/specs/misc/add-store.spec.js
+++ test/specs/misc/add-store.spec.js
@@ -2,33 +2,26 @@ const LoginPage = require('../../pages/auth/login.page');
 const StorePage = require('../../pages/store/store.page');
 const HomePage = require('../../pages/home/home.page');
 
-let time = moment().format();
-let storeName = (time  + " " + Random.string());
-let storeCity = Random.city();
-let storeNumber = Random.integer6();
+const { credentials } = require('../../../environments/environment-variables')
+const store = require('../../../data/store-data')
+
 
 describe('Bonus Coupon', () => {
-    
     it('Logs in Netzon Admin with valid credentials', () => {
         LoginPage.open()
-        LoginPage.login(process.env.STAGING_ADMIN_EMAIL, process.env.STAGING_ADMIN_PASS);  
+        LoginPage.login(credentials, 'admin');  
     });
 
+    it('Navigates to store page', () => {
+        HomePage.btnMenuStore.click()
+        StorePage.btnAddStore.click()
+        expect(StorePage.title).toHaveText("Lägg till återförsäljare")
+    })
     it('Adds a store', () => {
-        HomePage.btnMenuStore.click();
-        StorePage.btnAddStore.click();
+        StorePage.addStore(store)
 
-        expect(StorePage.title).toHaveText("Lägg till återförsäljare");
-
-        StorePage.addStore(
-            storeName, // Store name parameter
-            storeCity, // Store city parameter
-            storeNumber, // Store number parameter
-        )
-
-        StorePage.storeLocator(storeName).isDisplayed();
     })
     it('LOGS STORE INFO', () => {
-        console.log(storeNumber);
+        console.log(store.number);
     });
 })
